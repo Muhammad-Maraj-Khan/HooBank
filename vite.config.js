ba13a1f65df9744d6af7875ab031c070402492f0
Muhammad-Maraj-Khan <mairajkhan11345@gmail.com>
2024-08-19 12:50:16 -0700

    Fix build issue

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, // Ensure this matches the port you're trying to access
+  build: {
+    outDir: 'dist',
   },
 });
